300c2e3d0d18be8765f9440c0eeecb9e93d2b5c1
maxwu1104 <maxwu1104@gmail.com>
2023-04-18 23:41:07 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const Todo = require('./models/todo')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
@@ -37,67 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 //setting router
-app.get('/', (req, res) => {
-  // get all todo data
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then((todos) => res.render('index', { todos }))
-    .catch((error) => console.error(error))
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  const todo = new Todo({ name })
-
-  return todo
-    .save()
-    .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch((error) => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch((error) => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then((todo) => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => {
-      res.redirect(`/todos/${id}`)
-    })
-    .catch((error) => console.log('error'))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then((todo) => todo.deleteOne())
-    .then(() => res.redirect('/'))
-    .catch((error) => console.log('error'))
-})
+app.use(routes)
 
 //start and listen the server
 app.listen(port, () => {
