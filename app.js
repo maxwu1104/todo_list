51190db89eb6b60e33e06eadb919a58039fe7347
maxwu1104 <maxwu1104@gmail.com>
2023-04-19 08:10:33 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
-// require
+// require external package
 const express = require('express')
-const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
+
+// require project internal files
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-const port = 3000
 
 ////setting view engine with handlebars
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', './views')
-
-////setting DB
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected')
-})
 
 //always through here
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -41,6 +23,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 //start and listen the server
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log(`App is running on http://localhost:3000`)
 })
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,23 +1,8 @@
 //require
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-
-////setting MONGODB
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected')
-
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
