0c3de24d423b31a35f4ceef436d82770536b7074
maxwu1104 <maxwu1104@gmail.com>
2023-04-22 19:57:17 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -48,4 +48,13 @@ router.post('/register', (req, res) => {
     .catch((err) => console.log(err))
 })
 
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/users/login')
+  })
+})
+
 module.exports = router
