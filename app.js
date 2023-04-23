a39ad147e6665fc2bd13a80b8ee375b28e0adb4d
maxwu1104 <maxwu1104@gmail.com>
2023-04-23 16:25:37 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 // require project internal files
 const routes = require('./routes')
@@ -32,9 +33,13 @@ app.use(methodOverride('_method'))
 // user authentication function
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 
