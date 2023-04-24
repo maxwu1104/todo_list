8ebe9c2ae18175cc5a09532a048d73f1244863ac
maxwu1104 <maxwu1104@gmail.com>
2023-04-24 20:23:48 +0800

    refactor: change app port

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 app.use(routes)
 
 // start and listen the server
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
